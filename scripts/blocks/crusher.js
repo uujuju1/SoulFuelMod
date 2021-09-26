df14d0b1d4165e9f6aca21c2942ee138151a22e6
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-09-25 21:00:40 -0300

    better crusher

--- scripts/blocks/crusher.js
+++ scripts/blocks/crusher.js
@@ -4,38 +4,13 @@ const crusher = extend(GenericCrafter, "crusher", {
 		this.region = Core.atlas.find("ugly-fuel-crusher");
 		this.bottomRegion = Core.atlas.find("ugly-fuel-crusher-bottom");
 		this.middleRegion = Core.atlas.find("ugly-fuel-crusher-crusher");
-	},
-    generateIcons(){
-        return [
-            Core.atlas.find(this.name),
-            Core.atlas.find(this.name + "-bottom"),
-        ];
-    }
+	}
 });
 crusher.buildType = () => extend(GenericCrafter.GenericCrafterBuild, crusher, {
-	crusher1x: 0,
-	crusher2x: 0,
-	alt: false,
 	draw(){
 		Draw.rect(crusher.bottomRegion, this.x, this.y, 0)
-		Draw.rect(crusher.middleRegion, this.x + this.crusher1x, this.y, 0);
-		Draw.rect(crusher.middleRegion, this.x + this.crusher2x, this.y, 180);
+		Draw.rect(crusher.middleRegion, this.x + Angles.trnsx(this.totalProgress * 360, 0.5), this.y, 0);
+		Draw.rect(crusher.middleRegion, this.x + Angles.trnsx(this.totalProgress * 360, -0.5), this.y, 180);
 		Draw.rect(crusher.region, this.x, this.y, 0);
-	},
-	updateTile(){
-		this.super$updateTile();
-		if (this.crusher1x <= -0.9) {
-			this.alt = true;
-		};
-		if (this.crusher1x >= 0.9) {
-			this.alt = false;
-		};
-		if (this.alt) {
-			this.crusher1x+= 0.5;
-			this.crusher2x-= 0.5;
-		} else {
-			this.crusher1x-= 0.5;
-			this.crusher2x+= 0.5;
-		};
 	}
-})
+});
